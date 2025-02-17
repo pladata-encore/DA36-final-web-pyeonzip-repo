eb2604d07786847410dc2d6291532874b5139594
kimheea <khekhezx0519@gmail.com>
2025-02-17 10:42:36 +0900

    Feat: setting.py 분리

---
+++ pyeonzip/pyeonzip/settings_dev.py
@@ -0,0 +1,7 @@
+from .settings import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# DB 관련 세팅
---
+++ pyeonzip/pyeonzip/settings_prod.py
@@ -0,0 +1,9 @@
+from .settings import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = [
+'Eb-pyeonzip-app-env.eba-skazjekk.ap-northeast-2.elasticbeanstalk.com',
+]
+
+# DB 관련 세팅
