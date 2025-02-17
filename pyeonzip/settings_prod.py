ca78c039591ad4da4b4c52a09c2b7764f3272118
kimheea <khekhezx0519@gmail.com>
2025-02-17 15:17:54 +0900

    Update settings_prod.py

--- pyeonzip/settings_prod.py
+++ pyeonzip/settings_prod.py
@@ -1,4 +1,5 @@
 from .settings import *
+import socket
 
 DEBUG = False
 
@@ -6,5 +7,12 @@ ALLOWED_HOSTS = [
     'Eb-pyeonzip-app-env.eba-skazjekk.ap-northeast-2.elasticbeanstalk.com',
     'pyeonzip.store'
 ]
+
+# 내부 ip추가
+try:
+    internal_ip = socket.gethostbyname(socket.gethostname())
+    ALLOWED_HOSTS.append(internal_ip)
+except Exception as e:
+    print(str(e))
 
 # DB 관련 세팅
