baa21094d597bbe5e6319f97def81fcd6e8f8c89
kimheea <khekhezx0519@gmail.com>
2025-02-17 11:49:38 +0900

    Feat: 장고 프로젝트 밖으로 꺼내기

--- pyeonzip/settings.py
+++ pyeonzip/settings.py
@@ -12,7 +12,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+# from dotenv import load_dotenv
 
+# .env 파일 로드
+# load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -26,7 +29,9 @@ SECRET_KEY = 'django-insecure-4pv*_6d7um$pxjnc84gl2ujw!zcyaol5y4y+%1ijp!)&cql$m^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '3.34.109.55',
+]
 
 
 # Application definition
@@ -38,11 +43,22 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     'review',
     'product',
-    'oauth',
-    'community'
+    'users',
+    'community',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.naver',
+    'allauth.socialaccount.providers.kakao',
 ]
+
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -52,6 +68,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'pyeonzip.urls'
@@ -73,6 +91,7 @@ TEMPLATES = [
     },
 ]
 
+
 WSGI_APPLICATION = 'pyeonzip.wsgi.application'
 
 
@@ -82,19 +101,16 @@ WSGI_APPLICATION = 'pyeonzip.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'pyeonzip',
-        'USER': 'cy',
-        'PASSWORD': 'cy',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
         'OPTIONS': {
             'charset': 'utf8mb4',
         }
     }
 }
-
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
@@ -129,14 +145,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
+# BASE_DIR = Path(__file__).resolve().parent.parent
 
-STATIC_URL = 'static/'
-
-# staticfiles 디렉토리 지정
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
-
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# SITE_ID = 1
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
@@ -148,6 +163,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # (기본값: /accounts/profile/)
 LOGIN_REDIRECT_URL = '/'
 
+SOCIALACCOUNT_LOGIN_ON_GET = True
 # 파일 업로드를 위한 설정
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
