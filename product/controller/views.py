7da3334a9bf8532c4b10df1c3b9642b1cb0b2d06
chaeyeon-h <jackie0912@naver.com>
2025-02-20 14:02:18 +0900

    Update: 상품 filter 기능 수정

--- product/controller/views.py
+++ product/controller/views.py
@@ -14,31 +14,60 @@ product_service=ProductServiceImpl.get_instance() # 객체 생성
 review_service=ReviewServiceImpl.get_instance()
 # Create your views here.
 
-
-
-
-
-
-def all_product_pagination(request):
-        # question service로부터 받아옴
-        product = product_service.find_all()
-        page = request.GET.get('page', 1)
-        paginator = Paginator(product, 20)  # 한페이지에 몇개씩?
-        page_obj = paginator.get_page(page)
-        last_page=paginator.num_pages
-
-        return render(request, 'product/product_list.html', context={'page_obj': page_obj,'last_page':last_page})
-
-
-def latest_product_pagination(request):
-    # question service로부터 받아옴
-    product = product_service.latest_product()
+def product_main(request):
+    product = product_service.find_all()
     page = request.GET.get('page', 1)
     paginator = Paginator(product, 20)  # 한페이지에 몇개씩?
     page_obj = paginator.get_page(page)
     last_page = paginator.num_pages
+    return render(request,"product/product_list.html",context={'page_obj': page_obj,'last_page':last_page})
 
-    return render(request, 'product/product_list.html', context={'page_obj': page_obj, 'last_page': last_page})
+
+def product_list(request,tab="all"):
+    print(tab)
+    product = product_service.find_all()
+    page = request.GET.get('page', 1)
+    paginator = Paginator(product, 20)  # 한페이지에 몇개씩?
+    page_obj = paginator.get_page(page)
+    last_page=paginator.num_pages
+
+    if tab == 'latest':
+        # latest_product_tab에 대한 HTML 조각 렌더링
+        product = product_service.latest_product()
+        page = request.GET.get('page', 1)
+        paginator = Paginator(product, 20)  # 한페이지에 몇개씩?
+        page_obj = paginator.get_page(page)
+        last_page = paginator.num_pages
+        print("product",product)
+
+    elif tab == 'ai':
+        pass
+
+    return render(request, 'product/all_product.html',context={'page_obj': page_obj,'last_page':last_page,"tab":tab})
+
+
+
+
+# def all_product_pagination(request):
+#         # question service로부터 받아옴
+#         product = product_service.find_all()
+#         page = request.GET.get('page', 1)
+#         paginator = Paginator(product, 20)  # 한페이지에 몇개씩?
+#         page_obj = paginator.get_page(page)
+#         last_page=paginator.num_pages
+#
+#         return render(request, 'product/product_list.html', context={'page_obj': page_obj,'last_page':last_page})
+#
+#
+# def latest_product_pagination(request):
+#     # question service로부터 받아옴
+#     product = product_service.latest_product()
+#     page = request.GET.get('page', 1)
+#     paginator = Paginator(product, 20)  # 한페이지에 몇개씩?
+#     page_obj = paginator.get_page(page)
+#     last_page = paginator.num_pages
+#
+#     return render(request, 'product/all_product.html', context={'page_obj': page_obj, 'last_page': last_page})
 
 def product_detail(request,product_id):
         product = product_service.find_by_id(product_id)
@@ -66,8 +95,7 @@ def product_likes(request, product_id):
         }, status=400)
 
 
-def filter_products(request):
-    store = request.GET.get("store", "ALL")  # store 요청 가져요기 _ 기본값=ALL
+def filter_products(request,store="ALL"):
     print(store)
     if store == "ALL":
         products = Product.objects.all()  # 전체 상품
@@ -78,22 +106,24 @@ def filter_products(request):
     paginator = Paginator(products, 20)  # 한페이지에 몇개씩?
     page_obj = paginator.get_page(page)
     last_page = paginator.num_pages
-    product_list = [
-        {
-            "id": product.product_id,
-            "name": product.product_name,
-            "price": product.product_price,
-            "store": product.convenient_store_name,
-            "image": product.product_image_url if product.product_image_url else "/static/images/logo.png",
-            "likes": product.likes.count(),
-            "reviews": product.Product_reviews.count(),
-        }
-        for product in page_obj
-    ]
-
-    data = {
-        "products": product_list,  # 상품 리스트
-        "total_pages": paginator.num_pages,  # 총 페이지 수
-        "current_page": page_obj.number,  # 현재 페이지
-    }
-    return JsonResponse(data)
+    # product_list = [
+    #     {
+    #         "id": product.product_id,
+    #         "name": product.product_name,
+    #         "price": product.product_price,
+    #         "store": product.convenient_store_name,
+    #         "image": product.product_image_url if product.product_image_url else "/static/images/logo.png",
+    #         "likes": product.likes.count(),
+    #         "reviews": product.Product_reviews.count(),
+    #     }
+    #     for product in page_obj
+    # ]
+    #
+    # data = {
+    #     "products": product_list,  # 상품 리스트
+    #     "total_pages": paginator.num_pages,  # 총 페이지 수
+    #     "current_page": page_obj.number,  # 현재 페이지
+    # }
+    # return JsonResponse(products)
+    #
+    return render(request, 'product/filter_page_list.html', context={'page_obj': page_obj,'last_page':last_page,"store":store})
